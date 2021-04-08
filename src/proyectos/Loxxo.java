840fea4c34b1ca00ecc7e85aa9b1d596f738116a
Ricardo <ricasdo987@gmail.com>
2021-04-08 01:42:02 -0500

    Solo Proyecto

--- src/proyectos/Loxxo.java
+++ src/proyectos/Loxxo.java
@@ -1,69 +1,59 @@
-
 package proyectos;
 
 import java.util.Scanner;
-        
+
 public class Loxxo {
-    
+
     public static void main(String[] args) {
-        
+
         Scanner sn = new Scanner(System.in);
-   boolean salir = false;
-   int opcion;
-   
-   while(!salir){
+        boolean salir = false;
+        int opcion;
+        int codigoCliente;
+        String nombre;
+        String apellidopaterno;
+        String apellidomaterno;
+        String telefono;
+        String direccion;
+        int edad;
+      
         System.out.println("1- AGREGAR CLIENTE");
         System.out.println("2- MOSTRAR CLIENTES");
-        System.out.println("3- BUSCAR");
-        System.out.println("4- APELLIDO MATERNO");
-        System.out.println("5- TELEFONO");
-        System.out.println("6- DIRECCIÓN");
-        System.out.println("7- EDAD");
-    
-        System.out.println("Elige una opcion");
+        System.out.println("3- BUSCAR CLIENTE");
+        System.out.println("4- CERRAR SECION");
+
+        System.out.println("----------------------------------");
+        System.out.println("Elige una opcion: ");
         opcion = sn.nextInt();
-        
-        switch(opcion){
-            
+        System.out.println("-----------------------------------");
+
+
+switch (opcion) {
             case 1:
-                
-        System.out.println("Ingresa datos del cliente");
-         System.out.println("1- INGRESA CODIGO DEL CLIENTE");
-        System.out.println("2- NOMBRE DEL CLIENTE");
-        System.out.println("3- APELLIDO PATERNO");
-        System.out.println("4- APELLIDO MATERNO");
-        System.out.println("5- TELEFONO");
-        System.out.println("6- DIRECCIÓN");
-        System.out.println("7- EDAD");
-        
-       
+                System.out.println("1- INGRESA CODIGO DEL CLIENTE: ");
+                codigoCliente = sn.nextInt();
+
+                System.out.println("2- NOMBRE DEL CLIENTE: ");
+                nombre = sn.nextLine();
+
+                System.out.println("3- APELLIDO PATERNO: ");
+                apellidopaterno = sn.nextLine();
+
+                System.out.println("4- APELLIDO MATERNO: ");
+                apellidomaterno = sn.nextLine();
+
+                System.out.println("5- TELEFONO: ");
+                telefono = sn.nextLine();
+
+                System.out.println("6- DIRECCIÓN: ");
+                direccion = sn.nextLine();
+
+                System.out.println("7- EDAD: ");
+                edad = sn.nextInt();
+
                 break;
-            case 2:
-                
-                break;
-            case 3:
-            
-                break;
-            case 4:
-            
-            
-                break;
-            case 5:
-                
-                break;
-            case 6:
-                
-                break;
-                
-            case 7:
-                
-                break;
-               
-                
+           
         }
     }
-    
-    }
-    
-    
+
 }
