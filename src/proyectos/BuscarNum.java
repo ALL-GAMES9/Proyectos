a706e5528a9b6ec7249a15482271bd97fb53eaf4
Ricardo <ricasdo987@gmail.com>
2021-04-09 14:15:53 -0500

    Listo

--- src/proyectos/BuscarNum.java
+++ src/proyectos/BuscarNum.java
@@ -1,52 +1,39 @@
 package proyectos;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class BuscarNum {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int arreglo[], numero;
-        boolean creciente = true;
-        arreglo = new int[10]; //el arreglo es de 10 posiciones
+        Scanner sc = new Scanner(System.in);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num;
+        int tam[];
 
-        do {
-            //pedimos el arreglo
-            System.out.println("Rellene el arreglo");
-            for (int i = 0; i < 10; i++) {
-                System.out.println(i + " Digite un numero: ");
+        tam = new int[10];
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i + ".-Ingrese un numero:");
+            tam[i] = sc.nextInt();
+        }
+        System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
+        System.out.println("¿QUE NUMERO DESEA BUSCAR?");
+        num = sc.nextInt();
+        int i = 0;
 
-                arreglo[i] = entrada.nextInt();
-            }
-            //verificar si el arreglo  esta ordenado
-
-            for (int i = 0; i < 9; i++) {
-                if (arreglo[i] < arreglo[i + 1]) {//creciente: 1,2,3...
-                    creciente = true;
-                }
-
-            }
-
-            //pedimos el numero a buscar 
-            System.out.println("\nIntroduzca el numero a buscar: ");
-            numero = entrada.nextInt();
-
-            //buscamos el numero en el  arreglo
-            int i = 0;
-
-             while (i < 10 && arreglo[i] < numero) {
-                i++;
-            }
-
-            if (i == 10) {//hemos reocorrido todo el arreglo y no encontramos nada
-                System.out.println("\n -1");
-            } else {
-                if (arreglo[i] == numero) {
-
-                    System.out.println("\nNumero encontrado, en la posición: " + i);
-                }
-            }
+        while (i < 10 && tam[i] < num) {
+            i++;
         }
         
+        if(i==10){
+            System.out.println("EL NUMERO A BUSCAR NO SE ENCUENTRA DENTRO DE LA LISTA:" + "-1");
+        }
+        else{
+        if(tam[i]==num){
+            System.out.println("El numero seleccionado esta en la posición: " + i);
+        }
+        }
+       
     }
 }
